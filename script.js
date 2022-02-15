59d4a8606e8bbf79ed73123237114ae98b4b8f6b
LouisLonchamp <lchp.louis@gmail.com>
2022-02-15 14:52:12 +0100

    Fixed some display problems and improved the program

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-/* Add variables */
+// Add variables //
 var gameStarted, dice, diceDOM, playerTurn, roundScore, globalScore;
 
 init();
 
-/* Principle of the roll dice button */
-document.getElementById('btn-roll-dice').addEventListener('click', function() {
+// Principle of the roll dice button //
+document.querySelector('.btn-roll-dice').addEventListener('click', function() {
     if(gameStarted) {
-        /* Roll of the dice */
-        dice = Math.floor(Math.random() *6) + 1;
+        // Roll of the dice //
+        dice = Math.floor(Math.random() * 6) + 1;
 
-        /* Display the right dice according to the roll */
-        diceDOM = document.getElementById('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        // Display the right dice according to the roll //
+        diceDOM = document.getElementById('dice');
+        diceDOM.src = 'images/dice-' + dice + '.png';
 
-        /* Each roll is added to the score of the round, if the player rolls the number 1, the round is cancelled */
+        // Each roll is added to the score of the round, if the player rolls the number 1, the round is cancelled //
         if (dice !== 1) {
             roundScore += dice;
             document.getElementById('current-' + playerTurn).textContent = roundScore; 
@@ -25,19 +24,18 @@ document.getElementById('btn-roll-dice').addEventListener('click', function() {
     }
 });
 
-/* Principle of the hold button */
-document.getElementById('btn-hold').addEventListener('click', function() {
+// Principle of the hold button //
+document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gameStarted) {
-        /* Adding the roundScore to globalScore */
+        // Adding the roundScore to globalScore //
         globalScore[playerTurn] += roundScore;
         document.getElementById('score-' + playerTurn).textContent = globalScore[playerTurn];
 
-        /* Winner or not */
+        // Winner or not //
         if (globalScore[playerTurn] >= 100) {
-            document.getElementById('name-' + playerTurn).textContent = 'Game won';
-            document.getElementById('.dice').style.display = 'none';
-            document.getElementById('.player-' + playerTurn + '-screen').classList.add('winner');
-            document.getElementById('.player-' + playerTurn + '-screen').classList.remove('activeturn');
+            document.getElementById('name-' + playerTurn).textContent = 'GAME WIN';
+            document.getElementById('player-' + playerTurn + '-screen').classList.add('winner');
+            document.getElementById('player-' + playerTurn + '-screen').classList.remove('activeturn');
             gameStarted = false;
         }
         else {
@@ -46,38 +44,41 @@ document.getElementById('btn-hold').addEventListener('click', function() {
     }
 });
 
-/* Functions */
+// Functions //
 
 function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    if (playerTurn == 0) {
+        playerTurn = 1
+    } 
+    else {
+        playerTurn = 0
+    }
     roundScore = 0;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.getElementById('.player-0-panel').classList.toggle('activeturn');
-    document.getElementById('.player-1-panel').classList.toggle('activeturn');
-    document.getElementById('.dice').style.display = 'none';
+    document.querySelector('.player-0-screen').classList.toggle('activeturn');
+    document.querySelector('.player-1-screen').classList.toggle('activeturn');
 }
 
-document.getElementById('.btn-new-game').addEventListener('click', init);
+document.querySelector('.btn-new-game').addEventListener('click', init);
 
 function init() {
-    /* Initial parameters */
+    // Initial parameters //
     globalScore = [0, 0];
     playerTurn = 0;
     roundScore = 0;
     gameStarted = true;
 
-    document.getElementById('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-0').textContent = 'player 1';
-    document.getElementById('name-1').textContent = 'player 2';
-    document.getElementById('.player-0-screen').classList.remove('winner');
-    document.getElementById('.player-1-screen').classList.remove('winner');
-    document.getElementById('.player-0-screen').classList.remove('activeturn');
-    document.getElementById('.player-1-screen').classList.remove('activeturn');
-    document.getElementById('.player-0-screen').classList.add('activeturn');
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-screen').classList.remove('winner');
+    document.querySelector('.player-1-screen').classList.remove('winner');
+    document.querySelector('.player-0-screen').classList.remove('activeturn');
+    document.querySelector('.player-1-screen').classList.remove('activeturn');
+    document.querySelector('.player-0-screen').classList.add('activeturn');
 }
