c75ae696ac8f42793a47af305e182f350b130a1c
LouisLonchamp <lchp.louis@gmail.com>
2022-02-15 12:32:37 +0100

    Principle of the roll dice buttonPrinciple of the roll dice button, nextPlayer and init functions will be treated later

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+/* Add variables */
+var gameStarted, dice, diceDOM, playerTurn, roundScore, globalScore
+
+/* Principle of the roll dice button */
+init();
+
+document.getElementById('btn-roll-dice').addEventListener('click', function() {
+    if(gameStarted) {
+        /* Roll of the dice */
+        dice = Math.floor(Math.random() *6) + 1;
+
+        /* Display the right dice according to the roll */
+        diceDOM = document.getElementById('.dice');
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        /* Each roll is added to the score of the round, if the player rolls the number 1, the round is cancelled */
+        if (dice !== 1) {
+            roundScore += dice;
+            document.getElementById('current-' + playerTurn).textContent = roundScore; 
+        }
+
+        else {
+            nextPlayer();
+        }
+    }
+});
 
